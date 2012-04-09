2c3cbed2a7d223896ebf19f5974d473b86ea50c0
maximusorg <admin@mstag.ru>
2012-04-09 14:23:38 +0400

    add main classes

---
+++ net/minecraft/config.java
@@ -0,0 +1,26 @@
+package net.minecraft;
+
+public class config {
+	//Откуда скачивать
+	public static String urldown = "http://dl.dropbox.com/u/46588455/Files/";
+	//Название вашего лаунчера
+	public static String title   = "Mstag launcher";
+	//версия лаунчера
+	public static String version = "14";
+	//Авторизация
+	public static String auth    = "http://mstag.ru/login/auth.php";
+	//IP вашего сервера
+	public static String serverip = "s1.mstag.ru";
+	//Порт вашего сервера
+	public static String serverport = "25565";
+	//Загрузка новостей
+	public static String news    = "http://mstag.ru/news/index.php";
+	//Регистрация
+	public static String register = "mstag.ru/index.php?do=register";
+	//Откуда скачивать новый лаунчер
+	public static String newlauncher = "http://mstag.ru/";
+	
+	
+	
+
+}
